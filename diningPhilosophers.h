439b365b6d81c776ada8ce402219ecf1257b9df4
trizcard <beatrizco@usp.br>
2023-05-12 10:34:07 -0300

    comentando o header

--- diningPhilosophers.h
+++ diningPhilosophers.h
@@ -3,18 +3,19 @@
 
 #include <pthread.h>
 
-#define N 5
+#define N 5 // define a quantidade de filósofos
 
-typedef enum { THINKING, HUNGRY, EATING } State;
+typedef enum {THINKING, HUNGRY, EATING} State; // define todos os estados possíveis dos filósofos
 
+// cria a struct do monitor
 typedef struct {
-    State states[N];
-    pthread_mutex_t mutex;
-    pthread_cond_t conds[N];
+    State states[N]; // vetor que armazena um estado para cada filosofos 
+    pthread_mutex_t mutex; // mutex, que garante a exclusão mútua
+    pthread_cond_t conds[N]; // vetor de variáveis condicionais para sincronizar as threads.
 } Monitor;
 
-void init_monitor(Monitor *monitor);
-void take_chopsticks(Monitor *monitor, int i);
-void put_chopsticks(Monitor *monitor, int i);
+void init_monitor(Monitor *monitor); // inicaliza o monitor
+void take_chopsticks(Monitor *monitor, int i); // pegar os palitos 
+void put_chopsticks(Monitor *monitor, int i); // devolver os palitos
 
 #endif
